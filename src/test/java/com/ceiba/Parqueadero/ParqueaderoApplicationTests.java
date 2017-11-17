4af310954a5df9adb0e64aad38686abd1f311ff4
Sergio Vladimir Diaz Rolon - Ceiba Software <sergio.diaz@ceiba.com.co>
2017-11-17 15:47:20 -0500

    14c

--- src/test/java/com/ceiba/Parqueadero/ParqueaderoApplicationTests.java
+++ src/test/java/com/ceiba/Parqueadero/ParqueaderoApplicationTests.java
@@ -1,12 +1,24 @@
 package com.ceiba.parqueadero;
-
 import org.junit.Test;
+import com.ceiba.parqueadero.service.ParqueaderoServicio;
+import junit.framework.Assert;
 
 public class ParqueaderoApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void notifyTest() {
 		
+		
+		//arrange
+		int horas = 50;
+		double valor =1000;
+		double valorDia = 8000;
+		ParqueaderoServicio parqueaderoServicio =  new ParqueaderoServicio();
+		//act
+		double total = parqueaderoServicio.totalAPagar(horas,valor,valorDia);
+		//Assert
+		Assert.assertNotNull(total);		
+		System.out.println(total);
 	}
 
 }
